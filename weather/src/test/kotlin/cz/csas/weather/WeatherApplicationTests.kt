4010082632d9a232f2f4724db25c5cf078ef9af5
Miloš Popovič <popovmil@fit.cvut.cz>
2021-06-29 13:04:32 +0200

    Init Spring Boot project

---
+++ weather/src/test/kotlin/cz/csas/weather/WeatherApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.csas.weather
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
