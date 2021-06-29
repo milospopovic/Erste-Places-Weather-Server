3ac2564233a6110f43e2055d80189bf4e6e711a0
Miloš Popovič <popovmil@fit.cvut.cz>
2021-06-29 21:11:33 +0200

    Add clearing cache for current weather

---
+++ weather/src/main/kotlin/cz/csas/weather/service/ClearCacheTask.kt
@@ -0,0 +1,16 @@
+package cz.csas.weather.service
+
+import org.springframework.cache.CacheManager
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+class ClearCacheTask(private val cacheManager: CacheManager) {
+    /**
+     * Clears cache for storing current weather at the beginning of every hour
+     */
+    @Scheduled(cron = "0 0 * * * *")
+    fun clearCurrentWeatherCache(){
+        cacheManager.getCache("currentWeather")?.clear()
+    }
+}
